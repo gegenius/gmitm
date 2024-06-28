7c580c3fccae23174d9843e1a21ca0494df844cb
gegenius <125509896+gegenius@users.noreply.github.com>
2024-06-28 23:16:14 +0200

    fix errori stupidi

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from gmitm import *
+import time
 
 def elab(surceiface, packet):
     return FORWARD, packet, True
 
 try:
-    gmitm(elab, "eth0", "eth1")
-    gmitm.start()
+    g = gmitm(elab, "eth0", "eth1")
+    g.start()
     print("script avviato con successo")
+    while True:
+        time.sleep(10000)
 except KeyboardInterrupt:
-    gmitm.stop()
+    g.stop()
     print("script stoppato con successo")
