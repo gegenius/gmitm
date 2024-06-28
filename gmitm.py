35ba6ce88bf313f73cf453f696be7f0ef50cd076
gegenius <125509896+gegenius@users.noreply.github.com>
2024-06-28 23:14:44 +0200

    fix errori stupidi

--- gmitm.py
+++ gmitm.py
@@ -21,41 +21,38 @@ class gmitm():
         self.pcapfile = PcapWriter("./loot/" + datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S") + "-loot.pcap")
 
     def stop(self):
-        os.system("iptables -F")
-        os.system("iptables -P INPUT ACCEPT")
-        os.system("iptables -P OUTPUT ACCEPT")
-        os.system("iptables -P FORWARD ACCEPT")
+        os.system("iptables -D INPUT -j ACCEPT")
+        os.system("iptables -D OUTPUT -j ACCEPT")
+        os.system("iptables -D FORWARD -j ACCEPT")
         self.iface1.sniff.stop()
         self.iface2.sniff.stop()
 
     def start(self):
-        os.system("iptables -F")
-        os.system("iptables -P INPUT DROP")
-        os.system("iptables -P OUTPUT DROP")
-        os.system("iptables -P FORWARD DROP")
+        os.system("iptables -A INPUT -j DROP")
+        os.system("iptables -A OUTPUT -j DROP")
+        os.system("iptables -A FORWARD -j DROP")
 
         def initelab1(packet):
-            self.elaboration(self.iface1, packet)
-        self.iface1.sniff = AsyncSniffer(iface=self.iface1.name, pwd=initelab1, filter="inbound")
+            self.elaboration(self.iface1, self.iface2, packet)
+        self.iface1.sniff = AsyncSniffer(iface=self.iface1.name, prn=initelab1, filter="inbound")
 
         def initelab2(packet):
-            self.elaboration(self.iface2, packet)
-        self.iface2.sniff = AsyncSniffer(iface=self.iface2.name, pwd=initelab2)
+            self.elaboration(self.iface2, self.iface1, packet)
+        self.iface2.sniff = AsyncSniffer(iface=self.iface2.name, prn=initelab2, filter="inbound")
 
         self.iface1.sniff.start()
         self.iface2.sniff.start()
 
-    def elaboration(self, srcinterface, originpacket):
+    def elaboration(self, srciface, dstiface, originpacket):
         operation, elabpacket, save = self.elab(srcinterface.name, originpacket)
-        if operation == RESPOND:
-            srcinterface.socket.send(elabpacket)
-        elif operation == IGNORE:
+        try:
+            if operation == RESPOND:
+                srciface.sock.send(elabpacket)
+            elif operation == IGNORE:
+                pass
+            elif operation == FORWARD:
+                dstiface.sock.send(elabpacket)
+        except:
             pass
-        elif operation == FORWARD:
-            if srcinterface == self.iface1:
-                self.iface2.sock.send(elabpacket)
-            else:
-                self.iface1.sock.send(elabpacket)
-
         if save == True:
             self.pcapfile.write(elabpacket)
